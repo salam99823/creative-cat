214d06bd69c5ed93f967dfbb98f3f71648dd56ff
salam <salamatbekboev2008@gmail.com>
2025-03-02 11:14:01 +0900

    chore: change bg color

--- src/settings.rs
+++ src/settings.rs
@@ -1,9 +1,7 @@
 use bevy::prelude::*;
 
-pub const BACKGROUND_COLOR: Color = Color::srgb(0.545, 0.27, 0.074);
+pub const BACKGROUND_COLOR: Color = Color::srgb(0.4705, 0.3176, 0.1686);
 
 pub mod cat {
     pub const SPEED: f32 = 150.;
 }
-
-pub const LEVEL_SIZE: UVec2 = UVec2::new(10, 10);
